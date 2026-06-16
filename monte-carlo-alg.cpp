d22d57bfbc637b803c27a4ac3e247810e422519e
Drew Whitlow <whitlow@umich.edu>
2026-06-16 16:21:34 -0400

    added rest of project files to repo

---
+++ monte-carlo-alg.cpp
@@ -0,0 +1,45 @@
+#include "Game.h"
+
+
+// impliment "silent" variant of game for playouts
+// impliment means of creating silent game instance from last move of real game
+// build out graph of decisions based on satisfaction index at the end of the rollout
+// use a rough version of the heuristics-based model I wrote for the og AI to play moves during rollout
+// traverse graph with combination of 
+
+/*
+ *
+ * Game Files;
+ * Buidling.cpp
+ * Elevator.cpp
+ * Floor.cpp
+ * Game.cpp
+ * Move.cpp
+ * Person.cpp
+ * 
+ * Could we build the tree at compile time?
+ *
+ *
+*/
+
+// 
+
+class silent_game {
+    
+
+
+
+
+};
+
+
+int main() {
+
+    return 0;
+}
+
+
+
+
+
+
